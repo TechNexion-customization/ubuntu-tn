bfa605d86b9b156bac1186a39297d1a44b527794
Wig Cheng <wig.cheng@technexion.com>
2019-10-15 16:41:49 +0800

    env.bash: add xfce rootfs setup function to gen_pure_rootfs api

--- env.bash
+++ env.bash
@@ -1,20 +1,22 @@
+#!/bin/bash
+
 MODULE=$(basename $BASH_SOURCE)
 CPU_TYPE=$(echo $MODULE | awk -F. '{print $3}')
 UBUNTU_TYPE=$(echo $MODULE | awk -F. '{print $4}')
-
-
-
-
+TOP=${PWD}
 
 
 # generate minimum rootfs
 gen_pure_rootfs() {
-  TOP=${PWD}
   cp -rv cookers/tn_install.sh ${TOP}
   sudo debootstrap --arch=armhf --keyring=/usr/share/keyrings/ubuntu-archive-keyring.gpg --verbose --foreign bionic ${TOP}/rootfs
   sudo cp /usr/bin/qemu-arm-static ${TOP}/rootfs/usr/bin
   sudo LANG=C chroot ${TOP}/rootfs /debootstrap/debootstrap --second-stage
   sudo cp ${TOP}/tn_install.sh ${TOP}/rootfs/usr/bin/
+  sudo cp ${TOP}/apt_list ${TOP}/rootfs/usr/bin/
+
+
+  sudo cp -rv cookers/fs_overlay/etc/systemd/system/tn_init.service ${TOP}/rootfs/etc/systemd/system/
 
   if [[ "${UBUNTU_TYPE}" == "xfce" ]];then
     sudo LANG=C chroot ${TOP}/rootfs /bin/bash -c "chmod a+x /usr/bin/tn_install.sh; /usr/bin/tn_install.sh gui"
@@ -25,10 +27,19 @@ gen_pure_rootfs() {
   # fs-overlay
   sudo cp -rv ${TOP}/cookers/fs_overlay/usr/bin/* ${TOP}/rootfs/usr/bin/
   sudo cp -rv ${TOP}/cookers/fs_overlay/lib/modules/ ${TOP}/rootfs/lib/
-  for entry in $(ls ${TOP}/cookers/fs_overlay/usr/bin/); do
-    echo $entry
-  done
+  sudo cp -rv ${TOP}/cookers/fs_overlay/usr/lib/arm-linux-gnueabihf/* ${TOP}/rootfs/usr/lib/arm-linux-gnueabihf/
+  sudo rm -rf ${TOP}/rootfs/usr/lib/xorg/
+  sudo cp -rv ${TOP}/cookers/fs_overlay/usr/lib/xorg/ ${TOP}/rootfs/usr/lib/
+  sudo cp -rv ${TOP}/cookers/fs_overlay/usr/lib/dri ${TOP}/rootfs/usr/lib/
+  sudo cp -rv ${TOP}/cookers/fs_overlay/etc/X11/xorg.conf ${TOP}/rootfs/etc/X11/
+  sudo cp -rv ${TOP}/cookers/fs_overlay/etc/OpenCL ${TOP}/rootfs/etc/
+  sudo cp -rv ${TOP}/cookers/fs_overlay/etc/alternatives/arm-linux-gnueabihf_* ${TOP}/rootfs/etc/alternatives/
+  sudo cp -rv ${TOP}/cookers/fs_overlay/usr/include/* ${TOP}/rootfs/usr/include/
+  sudo cp -rv ${TOP}/cookers/fs_overlay/usr/share/* ${TOP}/rootfs/usr/share/
+  sudo cp -rv ${TOP}/cookers/fs_overlay/lib/firmware/ ${TOP}/rootfs/lib/
+  cp -rv ${TOP}/cookers/fs_overlay/home/ubuntu/.* ${TOP}/rootfs/home/ubuntu/
 
+  sync
 
   sudo rm -rf ${TOP}/rootfs/usr/bin/tn_install.sh
 
