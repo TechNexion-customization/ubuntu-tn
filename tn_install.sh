bacc09be62fda1877c83b17fed694075c8de7357
Wig Cheng <wig.cheng@technexion.com>
2019-10-15 16:35:34 +0800

    imx6: add xfce base environment setup to tn_install script

--- tn_install.sh
+++ tn_install.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 COL_GREEN="\e[1;32m"
 COL_NORMAL="\e[m"
@@ -26,14 +26,24 @@ yes "Y" | apt install openssh-server iw wpasupplicant hostapd util-linux procps 
 yes "Y" | apt install bash-completion
 
 echo "${COL_GREEN}Add swap partition...Default size is 1GB${COL_NORMAL}"
-dd if=/dev/zero of=/swapfile bs=1M count=1000
+dd if=/dev/zero of=/swapfile bs=1M count=256
 chmod 600 /swapfile
 mkswap /swapfile
 swapon /swapfile
 
 # GUI desktop support
 if [[ "$1" == "gui" ]];then
-yes "Y" | apt install xfce4 lightdm onboard
+yes "Y" | apt install xfce4 lightdm onboard glmark2 xterm xfce4-screenshooter rfkill
+packages=$(awk 'BEGIN { ORS = " " } { print }' apt_list)
+yes "Y" | apt install /usr/bin/$packages
+
+yes "Y" | apt remove xscreensaver gnome-terminal
+yes "Y" | apt-get autoremove
+
+sudo systemctl enable tn_init
+sudo systemctl start tn_init
+
+
 rm /usr/share/lightdm/lightdm.conf.d/50-unity-greeter.conf
 touch /usr/share/lightdm/lightdm.conf.d/50-xfce-greeter.conf
 cat <<END > /usr/share/lightdm/lightdm.conf.d/50-xfce-greeter.conf
@@ -44,8 +54,59 @@ allow-guest=false
 autologin-user=ubuntu
 autologin-user-timeout=0
 END
-fi
 
+mkdir -p /home/ubuntu/.config/xfce4/xfconf/xfce-perchannel-xml/
+chown ubuntu:ubuntu /home/ubuntu/.config/xfce4/xfconf/xfce-perchannel-xml/
+chown ubuntu:ubuntu /home/ubuntu/.config/xfce4/xfconf/
+chown ubuntu:ubuntu /home/ubuntu/.config/xfce4/
+chown ubuntu:ubuntu /home/ubuntu/.config/
+touch /home/ubuntu/xfce4/xfconf/xfce-perchannel-xml/xsettings.xml
+cat <<END > /home/ubuntu/.config/xfce4/xfconf/xfce-perchannel-xml/xsettings.xml
+<?xml version="1.0" encoding="UTF-8"?>
+
+<channel name="xsettings" version="1.0">
+  <property name="Net" type="empty">
+    <property name="ThemeName" type="empty"/>
+    <property name="IconThemeName" type="empty"/>
+    <property name="DoubleClickTime" type="empty"/>
+    <property name="DoubleClickDistance" type="empty"/>
+    <property name="DndDragThreshold" type="empty"/>
+    <property name="CursorBlink" type="empty"/>
+    <property name="CursorBlinkTime" type="empty"/>
+    <property name="SoundThemeName" type="empty"/>
+    <property name="EnableEventSounds" type="empty"/>
+    <property name="EnableInputFeedbackSounds" type="empty"/>
+  </property>
+  <property name="Xft" type="empty">
+    <property name="DPI" type="empty"/>
+    <property name="Antialias" type="empty"/>
+    <property name="Hinting" type="empty"/>
+    <property name="HintStyle" type="empty"/>
+    <property name="RGBA" type="empty"/>
+  </property>
+  <property name="Gtk" type="empty">
+    <property name="CanChangeAccels" type="empty"/>
+    <property name="ColorPalette" type="empty"/>
+    <property name="FontName" type="string" value="Sans 15"/>
+    <property name="MonospaceFontName" type="empty"/>
+    <property name="IconSizes" type="empty"/>
+    <property name="KeyThemeName" type="empty"/>
+    <property name="ToolbarStyle" type="empty"/>
+    <property name="ToolbarIconSize" type="empty"/>
+    <property name="MenuImages" type="empty"/>
+    <property name="ButtonImages" type="empty"/>
+    <property name="MenuBarAccel" type="empty"/>
+    <property name="CursorThemeName" type="empty"/>
+    <property name="CursorThemeSize" type="empty"/>
+    <property name="DecorationLayout" type="empty"/>
+  </property>
+</channel>
+END
+
+chown ubuntu:ubuntu /home/ubuntu/xfce4/xfconf/xfce-perchannel-xml/xsettings.xml
+
+yes "Y" | apt install --reinstall network-manager
+fi
 
 # clear the patches
 rm -rf var/cache/apt/archives/*
